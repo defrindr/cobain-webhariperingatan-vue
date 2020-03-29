03e7816a81801e6800f865e0de4df33977a1376c
defrindr <defrindr@gmil.com>
2020-03-29 18:07:50 +0700

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Axios from 'axios'
+
+Vue.prototype.$axios = Axios;
 
 Vue.config.productionTip = false
 
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
